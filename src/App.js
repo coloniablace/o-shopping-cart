50c23bbe67e19e945a5b0820cec38259e8958614
coloniablace <bcolonia0712@gmail.com>
2023-04-18 10:45:22 +0800

    path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/navbar";
-import { Shop } from "./pages/shop/shop";
-import { Contact } from "./pages/contact";
-import { Cart } from "./pages/cart/cart";
-import { ShopContextProvider } from "./context/shop-context";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/navbar';
+import { Shop } from './pages/shop/shop';
+import { Contact } from './pages/contact';
+import { Cart } from './pages/cart/cart';
+import { ShopContextProvider } from './context/shop-context';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
+            <Route path="/o-shopping-cart" element={<Shop />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
